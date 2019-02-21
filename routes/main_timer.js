d8a48bcb6fbe30a7dd875a8ed5316c8943a30124
Hillary <hthi@ucsd.edu>
2019-02-21 11:35:46 -0800

    Fixed  dynamically populating tasks based on subject; need to fix selected flag

--- routes/main_timer.js
+++ routes/main_timer.js
@@ -7,10 +7,24 @@ var data = require('../data.json');
 
 exports.view = function(req, res){
    var subjects=data.subjects
-   var tasks=data.subjects.tasks
+   //var tasks=data.subjects.tasks
+
+   var tasks = subjects.map( function(s) {
+     return s.tasks;
+ 	});
+
+   console.log("SUBJECTS");
+   console.log(subjects);
+   console.log("TASKS");
+   console.log(tasks);
    res.render('main_timer', {
      "subjects": subjects,
-     "tasks": tasks
+     "tasks": tasks,
+     "helpers": {
+            makeIdHandle: function (taskName) { 
+            	return taskName.replace(/\s/g,''); ; 
+            }
+        }
    });
 //  res.sendFile(path.join(__dirname+'/main_timer.html'));
 };
