3b3e5d40196688ef073936ee2c5da4bf4f9a91f4
Josh Robertson <jyrobert@ucsd.edu>
2019-02-22 03:59:25 -0800

    attempted to make back work for new subject

--- app.js
+++ app.js
@@ -67,18 +67,18 @@ app.use(bodyParser.json());
 
 app.post('/new_subject', function (req, res) {
 	var i;
-	var tasks = req.body.task;
-	for (i = 0; i < req.body.task.length; i++) {
-		var new_task = {'name': req.body.task[i]};
-		tasks[i] = new_task;
+	if (typeof req.body.task !== 'undefined') {
+		var tasks = req.body.task;
+		for (i = 0; i < req.body.task.length; i++) {
+			var new_task = {'name': req.body.task[i]};
+			tasks[i] = new_task;
+		}
 	}
 	var newSubj = {
 		'name': req.body.subject_name,
 		'tasks': tasks
 	};
 	data.subjects.push(newSubj);
-	console.log(data);
-	console.log(data.subjects);
 });
 
 app.post('/main_settings', function (req, res) {
