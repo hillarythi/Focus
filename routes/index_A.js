c2be08420b7679e5f3f259e662c96a8c632a21d1
Josephine Reyes <jgreyes@ucsd.edu>
2019-03-06 13:13:47 -0800

    GA

--- routes/index_A.js
+++ routes/index_A.js
@@ -6,3 +6,17 @@
 exports.view = function(req, res){
   res.render('index_A');
 };
+
+/*
+ * Function that is called when the document is ready.
+ */
+function initializePage() {
+  // your code here	
+	$("btn btn-default btn-lg home-icon left top dusty-pink").click(buttonClick);
+}
+
+function buttonClick(e) { 
+    e.preventDefault();
+ga("send", "event", "button", "click");
+
+}
